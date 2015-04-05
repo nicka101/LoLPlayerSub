71f17a380eb2f0962f951d8cf462260100b9ddc1
Nicka101 <nicka101@cloudbro.net>
2015-04-05 07:30:14 +0100

    * Initial Commit

---
+++ LoLPlayerSub/ButtonEnabled.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLPlayerSub
+{
+    public class ButtonEnabled : INotifyPropertyChanged
+    {
+        private bool _league_directory_valid = false;
+        private bool _serialized_data_valid = false;
+
+        public bool SubstitutionButtonEnabled
+        {
+            get { return LeagueDirectoryValid && SerializedDataValid; }
+        }
+
+        public bool LeagueDirectoryValid
+        {
+            get { return _league_directory_valid; }
+            set
+            {
+                _league_directory_valid = value;
+                OnPropertyChanged("LeagueDirectoryValid");
+                OnPropertyChanged("SubstitutionButtonEnabled");
+            }
+        }
+
+        public bool SerializedDataValid
+        {
+            get { return _serialized_data_valid; }
+            set
+            {
+                _serialized_data_valid = value;
+                OnPropertyChanged("SerializedDataValid");
+                OnPropertyChanged("SubstitutionButtonEnabled");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyName)
+        {
+            if (null != PropertyChanged)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ LoLPlayerSub/LogOutput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLPlayerSub
+{
+    public class LogOutput : INotifyPropertyChanged
+    {
+        ObservableCollection<string> _output = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Output
+        {
+            get
+            {
+                return _output;
+            }
+            set
+            {
+                _output = value;
+                OnPropertyChanged("Output");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyName)
+        {
+            if (null != PropertyChanged)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
